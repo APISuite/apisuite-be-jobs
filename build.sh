c726407e81a66fc3ff9e2155fc5fb1c6cd587499
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-02-10 19:55:27 +0000

    fix(build): fixed tags

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ HASH=$(git rev-parse --short HEAD)
 
 VERSION=$(cat package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
 
-docker build -t cloudokihub/apisuite-be-jobs:$HASH -t cloudokihub/apisuite-be-jobs:latest .
+docker build \
+  -t cloudokihub/apisuite-be-jobs:$HASH \
+  -t cloudokihub/apisuite-be-jobs:latest \
+  -t cloudokihub/apisuite-be-jobs:$VERSION .
+
 docker push cloudokihub/apisuite-be-jobs:$HASH
 docker push cloudokihub/apisuite-be-jobs:$VERSION
+docker push cloudokihub/apisuite-be-jobs:latest
